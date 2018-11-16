9f9b93efb179a4081c8058e678b414e3a2af1004
Artur Biniek <artur.biniek@gmail.com>
2018-11-16 21:50:04 +0100

    Wired up the Control Unit.

--- src/Tbx32.Core/XtdOpCode.cs
+++ src/Tbx32.Core/XtdOpCode.cs
@@ -4,6 +4,7 @@
     {
         // Extended format - OPCODE(6) | RA(5) | RB(5) | RC(5) | XTD (11)
         // First 16 values (0x0 to 0xF) reserved for ALU functions
+        // Currently only last 6 bits of XTD are used due to microcode decoding logic simplification. 7 bit address to lookup.
 
         Sub,
         Add,
